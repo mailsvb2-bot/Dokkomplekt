16d194e847a26b6456a77cd2afd1b408973b6aa2
BusinesMetro <mailsvb2@gmail.com>
2026-07-02 00:31:02 +0300

    Remove accidental quality probe

--- tests/test_quality_smoke_probe_v1494.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from smoke_quality_modernization import _missing_large_docstrings
-
-
-def test_quality_smoke_reports_no_missing_docstrings():
-    assert not _missing_large_docstrings()
