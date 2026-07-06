2ec4712bdf3d778866fa4266f0e06a41d7409bde
BusinesMetro <mailsvb2@gmail.com>
2026-07-06 18:30:43 +0300

    Expose PDF import through template setup entrypoint

--- window_universal_dialogs_mixin.py
+++ window_universal_dialogs_mixin.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
 from window_completion_dialog import prompt_regulatory_completion_values
-from window_mapper_dialog import open_template_setup_center, open_universal_document_mapper
+from window_mapper_dialog import open_universal_document_mapper
+from window_template_setup_pdf_wrapper import open_template_setup_center_with_pdf
 
 
 class WindowUniversalDialogsMixin:
     def _open_universal_document_mapper(self) -> None:
-        return open_template_setup_center(self)
+        return open_template_setup_center_with_pdf(self)
 
     def _open_first_run_create_buttons_popup(self) -> None:
-        return open_template_setup_center(self, first_run=True)
+        return open_template_setup_center_with_pdf(self, first_run=True)
 
     def _open_universal_document_mapper_advanced(self) -> None:
         return open_universal_document_mapper(self)
