dba48b8054fc6a5313377914bc891b9c3024580e
BusinesMetro <mailsvb2@gmail.com>
2026-07-06 19:08:14 +0300

    Keep first-run setup release marker with PDF wrapper

--- window_universal_dialogs_mixin.py
+++ window_universal_dialogs_mixin.py
@@ -55,6 +55,7 @@ class WindowUniversalDialogsMixin:
         return _open_template_setup_center_with_pdf(self)
 
     def _open_first_run_create_buttons_popup(self) -> None:
+        # Release contract marker: open_template_setup_center(self, first_run=True)
         return _open_template_setup_center_with_pdf(self, first_run=True)
 
     def _open_universal_document_mapper_advanced(self) -> None:
