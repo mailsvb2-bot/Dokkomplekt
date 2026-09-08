07a7d75ba9281a2a782c9de3d132891c109ba34d
BusinesMetro <mailsvb2@gmail.com>
2026-09-08 12:26:44 +0300

    Route native access through production publication boundary

--- product_access_native/__init__.py
+++ product_access_native/__init__.py
@@ -1,1 +1,8 @@
-from product_access.native import NativeProductAccessMixin
+from product_access.native import NativeProductAccessMixin as _NativeProductAccessMixin
+from product_access.production_boundary import ProductionAccessBoundaryMixin
+
+
+class NativeProductAccessMixin(ProductionAccessBoundaryMixin, _NativeProductAccessMixin):
+    """Compose the native license owner with the production publication guard."""
+
+    pass
